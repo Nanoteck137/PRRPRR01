33795ff3a95c30cba74afa094dcfb8049705987f
Nanoteck137 <patrik.millvik@gmail.com>
2019-03-21 12:45:48 +0100

    Added some comments and refactored the code

--- src/net/nanoteck137/programming/SecureDoors.java
+++ src/net/nanoteck137/programming/SecureDoors.java
@@ -5,23 +5,32 @@ import java.util.Scanner;
 
 public class SecureDoors {
 
+    /**
+     * Main function of the program
+     * @param args Command line arguments for the program
+     */
     public static void main(String[] args)
     {
+        //NOTE(patrik): Scanner for input
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
+        int numLogs = Integer.parseInt(scanner.nextLine());
 
+        //NOTE(patrik): List of the persons that entered the system
         ArrayList<String> list = new ArrayList<>();
 
-        while (n-- > 0)
+        while (numLogs-- > 0)
         {
+            //NOTE(patrik): Splits the input string to get a command and a name
             String[] line = scanner.nextLine().split(" ");
 
             String command = line[0];
             String name = line[1];
 
+            //NOTE(patrik): If the command is "entry" then add them to the list and check if they are already in the system then print ANOMALY
             if(command.equals("entry"))
             {
                 boolean found = false;
+                //NOTE(patrik): Loop through the list and check if the person is in the list
                 for(int i = 0; i < list.size(); i++) {
                     if(list.get(i).equals(name)) {
                         found = true;
@@ -35,8 +44,11 @@ public class SecureDoors {
                     list.add(name);
                     System.out.printf("%s entered\n", name);
                 }
+
+            //NOTE(patrik): If the command is "exit" then remove the person from the list
             } else if(command.equals("exit")) {
                 boolean found = false;
+                //NOTE(patrik): Loop through the list and check if the person is in the list
                 for(int i = 0; i < list.size(); i++) {
                     if(list.get(i).equals(name)) {
                         found = true;
